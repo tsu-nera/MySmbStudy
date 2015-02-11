dc7e52fb35b732a9eaa488b9d7df515791df939e
tsu-nera <fox10225fox@gmail.com>
2015-02-11 12:38:07 +0900

    add delete method

--- test/smbstudy/XCopyTest.java
+++ test/smbstudy/XCopyTest.java
@@ -1,20 +1,27 @@
 package smbstudy;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class XCopyTest {
 
+	// http://www.k-tanaka.net/cmd/xcopy.php
 	static final String XCOPY_Command = "xcopy";
+
+	@Before
+	public void setUp() {
+	}
 
 	@Test
 	public void testDownloadOneFolder() throws IOException, InterruptedException {
 		String source = "\\\\192.168.100.102\\共有\\foo";
-		String target = "C:\\app\\pleiades\\workspace\\smbstudy\\foo";
+		String target = "C:\\app\\pleiades\\workspace\\smbstudy\\data\\foo";
 
 		// Process Builder生成
 		// xcopy /e /i source target ディレクトリを再帰的にコピー
@@ -29,14 +36,16 @@ public class XCopyTest {
 		// プロセス完了待ち合わせ
 		process.waitFor();
 
+
+
 		// 結果の出力
 		// InputStream is = process.getInputStream();
 		// printInputStream(is);
 
-
+		delete(new File(target));
 	}
 
-	public static void printInputStream(InputStream is) throws IOException {
+	private void printInputStream(InputStream is) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
 		try {
 			for (;;) {
@@ -49,4 +58,21 @@ public class XCopyTest {
 		}
 	}
 
+	private void delete(File f){
+		if( f.exists()==false ){
+			return ;
+		}
+
+		if(f.isFile()){
+			f.delete();
+		}
+
+		if(f.isDirectory()){
+			File[] files=f.listFiles();
+			for(int i=0; i<files.length; i++){
+				delete( files[i] );
+			}
+			f.delete();
+		}
+	}
 }
